0fd63337b84f0a13ca1ddc9fa01d699d9adeb3f5
Chaeyoung <oco6029@naver.com>
2022-05-16 13:10:28 +0900

    0516 CHAENG

---
+++ app/src/main/java/com/example/termproject/InviteUser.java
@@ -0,0 +1,67 @@
+package com.example.termproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class InviteUser extends AppCompatActivity {
+
+    DatabaseReference reference;
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance("https://mobile-programming-91257-default-rtdb.asia-southeast1.firebasedatabase.app/");
+    private DatabaseReference mReference = mDatabase.getReference();
+    private DatabaseReference myRef;
+    String uid;
+    String rfName = "rfrf";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_user);
+
+        EditText user = findViewById(R.id.user);
+        String name = user.getText().toString();
+
+        Button invite = findViewById(R.id.button);
+        invite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mReference.child("USER").child(name).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        if (snapshot.hasChild("nickname")) {
+                            for (DataSnapshot dataSnapshot : snapshot.child("nickname").getChildren()) {
+                                String key = dataSnapshot.getKey();
+                                uid = "" + dataSnapshot.getValue();
+                                Log.d("subin", uid);
+
+                                RFData rf = new RFData();
+                                //chat.setNickname("user");
+                                rf.setName(rfName);
+                                mReference.child(uid).child("/RFList/").push().setValue(rf);
+                            }
+                        } else {
+                            Log.d("subin", name);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/termproject/signup.java
+++ app/src/main/java/com/example/termproject/signup.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -100,13 +101,17 @@ public class signup extends AppCompatActivity implements View.OnClickListener {
                             String uid = user != null ? user.getUid() : null;
                             String nickname = editTextNickname.getText().toString();
 
+
+                            Log.d("uid",uid);
+
                             if(uid != null){
                                 userData userInfo = new userData();
                                 userInfo.setUid(uid);
-                                userInfo.setNickname(nickname);
-                                mReference.child("/USER/").child(nickname).push().setValue(uid);
+                                userInfo.setNickname(uid);
+                                mReference.child("/USER/").child(nickname).push().setValue(userInfo);
 
-                            Toast.makeText(getApplicationContext(), "회원가입 성공",Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(getApplicationContext(), "회원가입 성공",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), uid,Toast.LENGTH_SHORT).show();
                             finish();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                             }
