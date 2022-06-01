bf90977a68c5766cfdf59ac5e8ab7121af3da9ad
Chaeyoung <oco6029@naver.com>
2022-06-01 13:11:31 +0900

    listview에 checkbox 넣는거 하는중

---
+++ app/src/main/java/com/example/termproject/domain/ListViewItem.java
@@ -0,0 +1,15 @@
+package com.example.termproject.domain;
+
+import android.graphics.drawable.Drawable;
+
+public class ListViewItem {
+    private String text;
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public String getText(){
+        return this.text;
+    }
+}
